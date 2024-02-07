04f0e2493bcd58ed73f8ffd22adbd8b0bcf2677c
root <root@ip-172-31-15-81.ap-south-1.compute.internal>
2024-02-07 11:51:04 +0000

    main commit

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-from fastapi import FastAPI, Request, Form
+from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
 templates = Jinja2Templates(directory="/code")
 
 @app.get("/")
-def form_post(request: Request):
+def get_form(request: Request):
     return templates.TemplateResponse('form.html', context={'request': request})
+
